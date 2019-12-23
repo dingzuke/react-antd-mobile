a176488878f3003fc95db04e097b0c0c900d3519
dingzuke <675129745@qq.com>
2019-12-23 14:05:55 +0800

    打包

---
+++ build/precache-manifest.bc122f09b6afe31c10d72ef4ac43a339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bf2adcd9897181e1b211",
+    "url": "/static/js/main.bf2adcd9.chunk.js"
+  },
+  {
+    "revision": "364fc43579ad1bb62db6",
+    "url": "/static/js/2.364fc435.chunk.js"
+  },
+  {
+    "revision": "bf2adcd9897181e1b211",
+    "url": "/static/css/main.c921afdf.chunk.css"
+  },
+  {
+    "revision": "364fc43579ad1bb62db6",
+    "url": "/static/css/2.93649a64.chunk.css"
+  },
+  {
+    "revision": "6d67127563d3e07e32fd752f1a97b597",
+    "url": "/index.html"
+  }
+];
